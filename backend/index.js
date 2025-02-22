b38f2556821d14871dca462f3e6556d9948e1454
ankita-shah1234 <ankitashah2025@gmail.com>
2025-02-22 23:03:17 +0530

    backend part

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,41 @@
 //const express = require('express')   // old way
-
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+
 dotenv.config({});
 
-
 const app = express();
-
-const server = app.listen(3000);
-
-server.on('error', (err) => {
-  if (err.code === 'EADDRINUSE') {
-    console.error('Port 3000 is already in use');
-    process.exit(1);
-  }
-});
-
-
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corOptions = {
-    origin:'http//localhost:5173',
+const corsOptions = {
+    origin:'http://localhost:5173',
     credentials:true
 }
 
-const PORT = process.env.PORT || 3001;
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+
+// api's
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
+
+
 
 app.listen(PORT,()=>{
-  connectDB();  
- console.log(`server running at port ${PORT}`);
+    connectDB();
+    console.log(`Server running at port ${PORT}`);
 })
